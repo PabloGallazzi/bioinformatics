35cb4297d0cd0c06d0925bdda5890fac1c361923
PabloGallazzi <pablogallazzi@gmail.com>
2017-11-04 16:52:34 -0300

    adds input from keyboard for ex4

--- src/main/java/com/pgallazzi/bioinformatics/Ex4.java
+++ src/main/java/com/pgallazzi/bioinformatics/Ex4.java
@@ -37,7 +37,26 @@ public class Ex4 {
                 blastOutput = fileContents.toString();
                 scanner.close();
                 final String[] hits = StringUtils.substringsBetween(blastOutput, "<Hit>", "</Hit>");
-                doSearch(hits, args[0], file.getName());
+                if (args.length == 0) {
+                    System.out.println("Searching at input file " + file.getName());
+                    System.out.println("Insert a pattern to search for, the minimum length is ten (no spaces) . Try this one QHEDFGDEGAANHNRLGGDYDSPGVQVFVYGRSVF or this one +K+++PR+KL or this one QYHDYSDDGWVNLNRQGFSYQCPQGQVIVAVRSIF");
+                    System.out.println("Type \"exit\" is you want to leave");
+                    while (true) {
+                        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                        final String pattern = br.readLine().trim().replace(" ", "");
+                        if ("exit".equals(pattern)) {
+                            break;
+                        }
+                        if (pattern.length() < 10) {
+                            System.out.println("The minimum length of the pattern to search for is ten (no spaces). Try this one QHEDFGDEGAANHNRLGGDYDSPGVQVFVYGRSVF or this one +K+++PR+KL or this one QYHDYSDDGWVNLNRQGFSYQCPQGQVIVAVRSIF");
+                        } else {
+                            doSearch(hits, pattern, file.getName());
+                            System.out.println("Done you can see the output at " + OUTPUT_FOLDER + file.getName().replace(XML, pattern + TXT));
+                        }
+                    }
+                } else {
+                    doSearch(hits, args[0], file.getName());
+                }
             } catch (final Exception exception) {
                 System.out.println("Error running Ex4 for file input file " + file.getName());
                 exception.printStackTrace();
@@ -46,7 +65,7 @@ public class Ex4 {
         });
     }
 
-    private static void doSearch(final String[] hits, final String pattern, final String fileName) throws Exception{
+    private static void doSearch(final String[] hits, final String pattern, final String fileName) throws Exception {
         final FileWriter writer = new FileWriter(new File(OUTPUT_FOLDER + fileName.replace(XML, pattern + TXT)));
         try {
             writer.write("Results for input " + fileName + " and pattern " + pattern + System.getProperty(LINE_SEPARATOR));
@@ -58,6 +77,7 @@ public class Ex4 {
                             writer.write("Match's accession " + accession + System.getProperty(LINE_SEPARATOR));
                             try {
                                 final URL uniprotFasta = new URL(String.format("http://www.uniprot.org/uniprot/%s.fasta", accession));
+                                System.out.println("Searching a sequence for accession " + accession + " this can take a while!");
                                 final ProteinSequence seq = FastaReaderHelper.readFastaProteinSequence(uniprotFasta.openStream()).get(accession);
                                 writer.write("Full sequence for accession " + accession + " " + seq + System.getProperty(LINE_SEPARATOR));
                             } catch (final Exception exception) {
